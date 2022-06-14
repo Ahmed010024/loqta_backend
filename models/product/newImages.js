c5d825935dc68a78e26d1a2be505c20b02e1574e
abdalstar <abdalstar@storkhost.com>
2022-06-14 13:16:33 +0200

    fix

--- models/product/newImages.js
+++ models/product/newImages.js
@@ -8,7 +8,7 @@ const getData = async (files) => {
         var type = name[name.length - 1];
         var newName = Math.round(Math.random() * 1000000) + "_" + Math.round(Math.random() * 1000000) + "_" + Math.round(Math.random() * 1000000) + "_" + Math.round(Math.random() * 1000000) + "." + type
         console.log(newName,"newNamenewNamenewNamenewNamenewNamenewName")
-        var newpath = './public/images/products/' + newName;
+        var newpath = '../public/images/products/' + newName;
         fs.rename(oldpath, newpath, function (err) {
             if (err) throw err;
         });
